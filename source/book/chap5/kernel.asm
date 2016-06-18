56f38376002ea14610e5929ce60ad2cca1d69cbc
james.boo <boojongmin@daum.net>
2016-06-19 08:29:38 +0900

    20160618 스터디 소스분석

--- source/book/chap5/kernel.asm
+++ source/book/chap5/kernel.asm
@@ -6,7 +6,7 @@
 start:
 
 
-	cld			; Advance = inc
+	cld			; Advance = inc -> direction inc
 	mov	ax,cs
 	mov	ds,ax
 	xor	ax,ax
@@ -15,18 +15,29 @@ start:
 
 	xor ebx, ebx
    	lea eax,[tss1]    ; EAX에 tss1의 물리주소를 넣는다.
-	add eax, 0x10000
-    	mov [descriptor4+2],ax
-    	shr eax,16
-   	mov [descriptor4+4],al
-    	mov [descriptor4+7],ah
+	add eax, 0x10000	; tss1 + 0x10000
+    	mov [descriptor4+2],ax  ; limit 2byte를 건너뛰기 위함.
+				; 상위 16를 접근할 수 없으니까 하위 16를 넣고 (ax만 접근 eax 풀로 사용 불가)
+    	shr eax,16		; base Address  
+				; eax right shift -> 상위 16비트의 값을 가져오기 위해 shr
+   	mov [descriptor4+4],al	; 1byte
+				; base address 16~ 23 값 
+    	mov [descriptor4+7],ah	; 1byte base address의 24~31
+				; base address를 가져옴.
 
-    	lea eax,[tss2]    ; EAX에 tss2의 물리주소를 넣는다.
+    	lea eax,[tss2]    ; EAX에 tss2의 물리주소를 넣는다. 
 	add eax, 0x10000
     	mov [descriptor5+2],ax
     	shr eax,16
     	mov [descriptor5+4],al
     	mov [descriptor5+7],ah
+
+    	;mov [descriptor5+2],ax
+    	;mov [descriptor5+4],al
+    	;mov [descriptor5+7],ah
+		
+
+; tss 초기화 종료
 
 	cli
         lgdt[gdtr]
@@ -54,14 +65,20 @@ PM_Start:
 
 	lea esp, [PM_Start]
 
-	mov ax, TSS1Selector
-	ltr ax
-	lea eax, [process2]
-	mov [tss2_eip], eax
-	mov [tss2_esp], esp
+	mov ax, TSS1Selector	; gdt index 4
+				; 현재 진행중인 task를  tss1이 가지고 있기위한 행위
+	ltr ax			; cpu tr register에 tss디스크립터 셀렉터 값을 넣는다.
+				; task switch를 할때 일부 값들은  cpu가 자동으로 저장  
+	lea eax, [process2]	; 그림5-6 TSS중 값
 
-	jmp TSS2Selector:0
-	;다시 태스크 스윗칭이 되면 이곳으로 돌아온다.
+	mov [tss2_eip], eax	; 실행될 task의 주소
+				; process2의 주소를  tss2_eip에 저장
+	mov [tss2_esp], esp 	; 현재 스택  포인터를 tss2_esp에 저장	
+				; 호출할 주소의 값을 초기화
+
+	jmp TSS2Selector:0	; p179 태스크 스위칭 흐름도 참조
+				; 32bit GDT를 이용하여 jmp
+				; 다시 태스크 스윗칭이 되면 이곳으로 돌아온다.
 
 	mov edi, 80*2*9
 	lea esi, [msg_process1]
@@ -96,7 +113,12 @@ process2:
 	mov edi, 80*2*7
 	lea esi, [msg_process2]
 	call printf
-	jmp TSS1Selector:0
+	jmp TSS1Selector:0		; selector가 tss이기 때문에 
+					; s비트가 system bit이때문에 cpu가 알아서
+					; 44bit - 70p에 s비트 설명이 나옴
+					; system bit는 0
+					; code/data  세그먼트 1 
+					; p197 5-9의 과정이 이루어짐 
 		
 ;***************************************
 ;**********   Data Area   **************
@@ -117,11 +139,11 @@ gdt:
 
 descriptor4:
 	dw 104
-	dw 0
-	db 0
-	db 0x89
-	db 0
-	db 0
+	dw 0			; tss 0
+	db 0			; 1
+	db 0x89	
+	db 0			; 
+	db 0			; 0
 
 descriptor5:
 	dw 104
@@ -176,5 +198,5 @@ tss2_esp:
     dw 0, 0         ; LDT, 사용안함
     dw 0, 0         ; 디버그용 T비트, IO 허가 비트맵
 
-times 1024-($-$$) db 0
+times 1024-($-$$) db 0		; sector 사이즈를 맞추기 위함.
 
