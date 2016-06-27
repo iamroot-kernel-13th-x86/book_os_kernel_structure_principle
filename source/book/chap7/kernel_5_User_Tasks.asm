e0d543b31dfc3442604eacfa1e3d835ee0fdc866
james.boo <boojongmin@daum.net>
2016-06-27 15:02:30 +0900

    20160625

--- source/book/chap7/kernel_5_User_Tasks.asm
+++ source/book/chap7/kernel_5_User_Tasks.asm
@@ -11,12 +11,12 @@ start:
 	mov	ss,ax
 
 	xor eax, eax
-   	lea eax,[tss]          ; EAX에 tss의 물리주소를 넣는다.
+  lea eax,[tss]          ; EAX에 tss의 물리주소를 넣는다.
 	add eax, 0x10000
-    	mov [descriptor4+2],ax
-    	shr eax,16
-   	mov [descriptor4+4],al
-    	mov [descriptor4+7],ah
+  mov [descriptor4+2],ax
+  shr eax,16
+  mov [descriptor4+4],al
+  mov [descriptor4+7],ah
 
 	xor eax, eax
 	lea eax, [printf]      ; EAX에 printf 함수의 주소를 넣는다.
@@ -27,21 +27,23 @@ start:
 	mov [descriptor7+7], ah	
 
 	cli
-        lgdt[gdtr]
+  lgdt[gdtr]
 
-        mov eax, cr0
-        or eax, 0x00000001
-        mov cr0, eax
+  mov eax, cr0
+  or eax, 0x00000001
+  mov cr0, eax
 
-        jmp $+2
+  jmp $+2
  	nop
 	nop
 
-        jmp dword SysCodeSelector:PM_Start
+  jmp dword SysCodeSelector:PM_Start
 
 [bits 32]
-	times 80 dd 0          ; 스택 영역을 만들어 놓는다.
-PM_Start:
+	times 80 dd 0         ;스택 영역을 만들어 놓는다. 더블워드로 80개: 커널스택으로 공간 할당
+												;원칙적으로 빈공간을 4바이트 단위로 채워줘야한다. 
+  
+	PM_Start:
 	
 	mov bx, SysDataSelector
 	mov ds, bx
@@ -77,8 +79,8 @@ PM_Start:
 	mov cx, 8
 	rep movsb
 
-	mov edi, 8*0x80         ; 트랩 IDT 디스크립터를 복사한다.
-	lea esi, [idt_soft_int]
+	mov edi, 8*0x80         ; 트랩 IDT 디스크립터를 복사한다. (콜게이트 대신 트랩(0x80)을 통해 유저->커널로 넘어가도록 설정)
+	lea esi, [idt_soft_int]	; 인터럽트가 걸릴때 여기로 넘어온다.
 	mov cx, 8
 	rep movsb
 
@@ -90,67 +92,81 @@ PM_Start:
 	
 	mov ax, TSSSelector
 	ltr ax
+	
 
-	mov eax, [CurrentTask]  ; Task Struct의 리스트를 만든다.
-	add eax, TaskList
-	lea edx, [User1regs]
-	mov [eax], edx  
-	add eax, 4
+	mov eax, [CurrentTask]  ; Task Struct의 리스트를 만든다. (처음 CurrentTask=0)
+	
+	;<--------------------TaskList 배열 값 저장 반복 ------------------->
+	add eax, TaskList				; TaskList의 첫번째 인덱스에 User1의 레지스터 영역을 저장한다.(TaskList: Task들이 가지고 있는 주소값을 담은 배열) 
+													; (ax를 배열의 인덱스 생각하는게 편함)
+	lea edx, [User1regs]		; User1regs 영역의 시작값을 edx에 저장
+	mov [eax], edx  				; TaskList[0] = [User1regs] (User1regs의 주소 값을 TaskList의 첫번째 인덱스에 저장한다)
+	
+	add eax, 4							
 	lea edx, [User2regs]
 	mov [eax], edx	
+	
 	add eax, 4
 	lea edx, [User3regs]
 	mov [eax], edx	
+	
 	add eax, 4
 	lea edx, [User4regs]
 	mov [eax], edx	
+	
 	add eax, 4
 	lea edx, [User5regs]
 	mov [eax], edx	
+	;<------------------------------------------------------------------>
+	
+	mov eax, [CurrentTask]  ; 실행할 첫 번째 Task를 선택한다. (CurrentTask = 0)
+	add eax, TaskList				
+	mov ebx, [eax]					; 이제 실행할 TaskList의 첫 번째 인덱스(User1regs)를 ebx에 두고
+	jmp sched								; sched로 점프한다.
 
-	mov eax, [CurrentTask]  ; 첫 번째 Task를 선택한다. (CurrentTask = 0)
-	add eax, TaskList
-	mov ebx, [eax]
-	jmp sched
 
-
-scheduler:
+scheduler:								;지금까지 커널을 사용하고 나서 커널을 빠져나가야 하는데, 
+													;이후 전환될 user_process에 대비하기 위해 현재 커널스택에 있는 
+													;각 유저 레지스터를 보관하기 위한 작업을 scheduler:에서 실행한다.
 
 	lea esi,[esp]           ; 커널 ESP에는 유저 레지스터들이 있다.
 	
-	xor eax, eax	
-	mov eax, [CurrentTask]
-	add eax, TaskList
+	xor eax, eax						
+	mov eax, [CurrentTask]	
+	add eax, TaskList				;태스크가 스위칭 되기 때문에, 커널 스택에 저장되있던 레지스터를 유저 레지스터 영역에 넣어주기 위해
 
 	mov edi,[eax]           ; 현재 실행 중인 태스크의 저장영역을 선택한다.
-
+	
 	mov ecx,17              ; 17개의 DWORD(68 BYTE) 모든 레지스터의 바이트 합.
 	rep movsd               ; 복사하고, 
 	add esp,68              ; 17개의 DWORD 만큼 스택을 되돌려 놓는다.
 
-	add dword [CurrentTask], 4	
-	mov eax, [NumTask]
-	mov ebx, [CurrentTask]
-	cmp eax, ebx
-	jne yet
-	mov byte [CurrentTask], 0
-yet:
+	add dword [CurrentTask], 4	;현재 태스크 인덱스에 4를 더해서 다음 인덱스로 넘어간다.
+	mov eax, [NumTask]					
+	mov ebx, [CurrentTask]			
+	cmp eax, ebx								;NumTask와 CurrentTask의 값을 비교`
+	jne yet											;같으면 첫번째 프로세스로 돌아가고, 아니면 다음 프로세스를 실행한다.
+	mov byte [CurrentTask], 0	
+
+yet:											;현재 태스크에 EBX 저장영역을 불러온다.(현재태스크가 다음태스크가 된다.)
 	xor eax, eax
-	mov eax, [CurrentTask]
-	add eax, TaskList
+	mov eax, [CurrentTask]			
+	add eax, TaskList						
 	mov ebx, [eax]
+
 sched:	
-	mov [tss_esp0],esp      ; 커널영역의 스택주소를 TSS에 기입해 둔다.
+	mov [tss_esp0],esp      ; 커널영역의 스택주소를 TSS에 기입해 둔다. (앞으로 TSS는 커널 스택을 불러올 때만 사용)
 
 	lea esp,[ebx]           ; EBX에는 다음 태스크의 저장영역의 주소가 있다.
 
-	popad                   ; EAX, EBX, ECX, EDX, EBP, ESI, EDI 를 복원한다.
+	popad                   ; EAX, EBX, ECX, EDX, EBP, ESI, EDI((첫번째만)전부 0으로 초기화된 상태) 를 복원한다.
 	pop ds                  ; DS, ES, FS, GS 복원한다.
 	pop es
 	pop fs
 	pop gs
-                                ; IRET 명령으로 EIP, CS, EFLAGS, ESP, SS 가 복원되고, 
-	iret                    ; 다음 유저 태스크로 스윗칭 된다.
+                          ; IRET 명령으로 EIP, CS, EFLAGS, ESP, SS 가 복원되고, 
+	iret                    ; 다음 유저 태스크로 스윗칭 된다. (해당 User의 EIP를 참조)
+													
 
 CurrentTask dd 0                ; 현재 실행 중인 태스크 번호
 NumTask dd 20                   ; 모든 태스크의 수
@@ -185,17 +201,19 @@ printf_end:
 ;********  유저 프로세스 루틴  *********
 ;***************************************
 user_process1:
-mov eax, 80*2*2+2*5
-lea ebx, [msg_user_process1_1]
-int 0x80
-mov eax, 80*2*3+2*5
-lea ebx, [msg_user_process1_2]
-int 0x80
-inc byte [msg_user_process1_2]
-jmp user_process1
+	mov eax, 80*2*2+2*5
+	lea ebx, [msg_user_process1_1]
+	int 0x80												;트랩 인터럽트 시작
+	
+	mov eax, 80*2*3+2*5
+	lea ebx, [msg_user_process1_2]
+	int 0x80		
+	
+	inc byte [msg_user_process1_2]
+	jmp user_process1
 		
-msg_user_process1_1 db "User Process1", 0
-msg_user_process1_2 db ".I'm running now.", 0
+	msg_user_process1_1 db "User Process1", 0
+	msg_user_process1_2 db ".I'm running now.", 0
 
 user_process2:
 mov eax, 80*2*2+2*35
@@ -312,22 +330,26 @@ tss_esp:
 ;****************************************
 ;******   User1 Task_Structure   ********
 ;****************************************
-times 63 dd 0                   ; 유저 스택영역
-User1Stack:
-User1regs:	
-dd 0, 0, 0, 0, 0, 0, 0, 0	; EDI, ESI, EBP, EBX, EDX, ECX, EBX, EAX
-                                ; POPA 명령으로 모두 POP된다.
-dw UserDataSelector, 0		; DS
+times 63 dd 0                   ; 유저 스택영역 (63개 지정)
+User1Stack:											; TSS 처럼 영역 지정
+User1regs:											
+dd 0, 0, 0, 0, 0, 0, 0, 0	; EDI, ESI, EBP, EBX, EDX, ECX, EBX, EAX	;범용레지스터만 push 함
+																;5장과 달리 tss 영역을 하나만 지정하고 각 유저별로 레지스터 영역을 별도로 할당
+																;(유저 프로세스)
+
+                                ; POPad 명령으로 모두 POP된다.
+dw UserDataSelector, 0		; DS	-->하위 16비트는 UserDataSelector(0x30+3) 값으로 채우고 나머지는 0으로 채움(워드 크기에 맞추기 위해)
 dw UserDataSelector, 0		; ES
 dw UserDataSelector, 0		; FS
 dw UserDataSelector, 0		; GS
                                                 
-dd user_process1                ; EIP
+dd user_process1          ; EIP	--> iret이 pop한 EIP 레지스터가 가리키고 있음.--> user_process1 레이블 영역으로 이동
 dw UserCodeSelector, 0		; CS
-dd 0x200			; EFLAGS (0x200 enables ints)
-dd User1Stack			; ESP
+dd 0x200									; EFLAGS (0x200 enables ints)
+dd User1Stack							; ESP
 dw UserDataSelector, 0		; SS
-                                ; IRET 명령으로 모두 POP 된다.
+													; DS~SS까지 push 후, 나중에 IRET 명령으로 모두 POP 된다.
+													
 ;****************************************
 ;******   User2 Task_Structure   ********
 ;****************************************
@@ -483,34 +505,38 @@ isr_33_keyboard:
 
 	jmp ret_from_int
 	
-isr_128_soft_int:
-	push gs
+isr_128_soft_int:						;0x80 트랩 인터럽트가 걸리고 여기로 넘어온다
+														;(유저 레벨로 돌아가기 위해 레지스터 cs,eip,esp,ss,EFLAGS는 push가 되어있는 상태)
+	push gs										;나머지 유저 레벨에서 사용했던 gs,fs,es,ds를 커널 스택에 push한다
 	push fs
 	push es
 	push ds
-	pushad
+	pushad										;EAX,ECX,EDX,EBX,ESP,EBP,ESI,EDI 순서로 push
+														;(나중에 pop할때는 esp는 pop되지 않음)
 
-	mov cx, SysDataSelector
+	mov cx, SysDataSelector		;커널 셀렉터로 초기화
 	mov DS, cx
 	mov ES, cx
 	mov FS, cx
 	mov GS, cx
 
-	mov edi, eax
-	lea esi, [ebx]
-	call printf
+	mov edi, eax							
+	lea esi, [ebx]			
+	call printf								;printf 호출
 
 	jmp ret_from_int
 
 ret_from_int:
 	xor eax, eax
-	mov eax, [esp+52]
-	and eax, 0x00000003
-	xor ebx, ebx
-	mov bx, cs
-	and ebx, 0x00000003
-	cmp eax, ebx
-	ja scheduler
+	mov eax, [esp+52]					;(244p 그림 7-5참고)CS 값을 eax에 복사
+	and eax, 0x00000003				;인터럽트가 걸리기 전의 cs의 rpl값을 구하기 위해 0x00000003과 and 연산
+	xor ebx, ebx							
+	mov bx, cs								;인터럽트가 걸린 후의 cs의 RPL이 00이며, 이를 bx에 저장.
+	and ebx, 0x00000003				;ebx값과 0x00000003를 and 연산한다.
+	cmp eax, ebx							;eax와 ebx를 비교 (eax>ebx)
+	ja scheduler							;eax와 ebx의 값이 다르면 scheduler로 넘어간다.
+														;같을 경우, popad로 넘어간다.
+														;(여기에서 같을 경우는 커널 영역에서의 인터럽트를 의미, 다를경우는 유저영역에서 인터럽트가 걸렸음을 의미한다.)
 
 	popad
 	pop ds
@@ -546,12 +572,13 @@ idt_keyboard:
 	db 0
 	db 0x8E
 	dw 0x0001
-idt_soft_int:
-	dw isr_128_soft_int
+idt_soft_int:					;서비스 루틴의 오프셋을 넣어주고 0x08로 세그먼트 셀렉터를 넣어서 디스크립터 설정
+	dw isr_128_soft_int	;오프셋
 	dw 0x08
 	db 0
 	db 0xEF
 	dw 0x0001
 
 times 4608-($-$$) db 0
+
 
